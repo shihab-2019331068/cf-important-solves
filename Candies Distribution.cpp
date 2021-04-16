719a454485277e3355cd21726398dc6b13a14b64
shihab-2019331068 <69476389+shihab-2019331068@users.noreply.github.com>
2021-04-16 20:31:06 +0600

    Update and rename a.cpp to Candies Distribution.cpp

--- Candies Distribution.cpp
+++ Candies Distribution.cpp
@@ -1,3 +1,33 @@
+/*
+C. Candies Distribution
+Mail.Ru Cup 2018 Round 1.
+https://codeforces.com/contest/1054/problem/C
+
+
+There are n children numbered from 1 to n in a kindergarten. Kindergarten teacher gave ai (1≤ai≤n) candies to the i-th child. Children were seated in a row in order
+from 1 to n from left to right and started eating candies.
+
+While the i-th child was eating candies, he calculated two numbers li and ri — the number of children seating to the left of him that got more candies than he and 
+the number of children seating to the right of him that got more candies than he, respectively.
+
+Formally, li is the number of indices j (1≤j<i), such that ai<aj and ri is the number of indices j (i<j≤n), such that ai<aj.
+
+Each child told to the kindergarten teacher the numbers li and ri that he calculated. Unfortunately, she forgot how many candies she has given to each child.
+So, she asks you for help: given the arrays l and r determine whether she could have given the candies to the children such that all children correctly calculated 
+their values li and ri, or some of them have definitely made a mistake. If it was possible, find any way how she could have done it.
+
+Solution:
+for all a[i]==x, l[i]+r[i]==f[x];
+count all possible f[x], if the number of f[x] is greater than n, then impossible.
+then decide the array as, a[i]=f[l[i]+r[i]];
+then check if 'a' contradcts with 'l' and 'r' or not.
+then print 'a'.
+*/
+
+
+
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -64,49 +94,4 @@ int main()
    for(i=1; i<=n; i++)
       cout<<a[i]<<" \n"[i==n];
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
